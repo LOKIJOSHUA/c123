df7c6cc5f107dad6323bc4e216fee93e2214c8e3
LOKIJOSHUA <114734534+LOKIJOSHUA@users.noreply.github.com>
2022-10-21 18:38:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,6 +20,18 @@ console.log('Posenet is Initalized');
 }
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("FF0000")
+    if(scoreLeftWrist > 0.2) 
+    {
+    circle(leftWirstX,leftWristY,20);
+    InnumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InnumberleftWristY);
+     leftWristY_divide_1000 = remove_decimals/1000;
+     volume = leftWristY_divide_1000 *2
+     document.getElementById("volume").innerHTML = "Volume = " + volume;
+     song.setVolume(volume);
+    }
 }
 function play()
 {
@@ -32,6 +44,8 @@ function gotPoses(results)
         if(results.length > 0)
         {
             console.log(results);
+            scoreLeftWrist = results[0].pose.keypoints[9].score;
+            console.log("scoreLeftWrist = " + scoreLeftWrist);
             leftWristX = results[0].pose.leftWrist.x;
             leftWristY = results[0].pose.leftWrist.y;
             console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY)
